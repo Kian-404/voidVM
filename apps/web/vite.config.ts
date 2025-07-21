c4f5e74fbec026f0821b728b063449a74f664ebf
kian-404 <tianjianen2015@gmail.com>
2025-07-21 19:55:15 +0800

    🐎 refactor(web): 更新依赖版本，配置Docker忽略文件和服务器代理

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,22 +1,23 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueDevTools from "vite-plugin-vue-devtools";
-import path from "path";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
   server: {
+    host: '0.0.0.0',
     proxy: {
-      "/api": {
-        target: "http://localhost:3000",
+      '/api': {
+        target: 'http://localhost:3030',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        rewrite: path => path.replace(/^\/api/, ''),
       },
     },
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-});
+})
